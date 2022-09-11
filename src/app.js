4c9314569c288aca17be73d1f2216e8dac30e66c
soniigx <soniagquin@gmail.com>
2022-09-11 01:19:45 -0400

    Styled the fonts and the background

--- src/app.js
+++ src/app.js
@@ -68,10 +68,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
